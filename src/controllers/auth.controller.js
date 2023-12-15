5aecba1e366970199c52b9a48c053507917c745a
Dikshant <dikkpsd@gmail.com>
2023-12-15 12:29:15 +0530

    Added Login Controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const User = require('../models/user.models')
-const {response_201, response_500} = require('../utils/responseCodes.utils')
+const {response_201, response_500, response_400, response_200} = require('../utils/responseCodes.utils')
 
 exports.signup = async (req, res) => {
     const user = new User({
@@ -21,3 +21,27 @@ exports.signup = async (req, res) => {
         return response_500(res, "Error creating user", err)
     }
 };
+
+exports.login = async(req, res) =>
+{
+    const user = await User.findOne({email: req.body.email}).exec();
+
+    try
+    {
+        return response_200(
+            res,
+            "User Logged In Successfully",
+            {
+                username: user.username,
+                email: user.email,
+                name: user.name,
+                id: user._id,
+                token: await user.generateToken()
+            }
+        )
+    }
+    catch(err)
+    {
+        return response_500(res, "Couldn't Generate a session. Please try again later", err)
+    }
+}
