9bd2a0294e3c4b636ed70b7e8c6cce8b6c0c034c
Jarviss77 <gupta.shubham2404@gmail.com>
2023-12-16 17:50:44 +0530

    Added isChanneladmin Middleware

--- src/middlewares/isChannelAdmin.middleware.js
+++ src/middlewares/isChannelAdmin.middleware.js
@@ -1,12 +1,10 @@
 import  Channel  from '../models/channel.models.js';
 import Workspace from '../models/workspace.models.js';
-import User from '../models/user.models.js';
 import USER_ROLE from '../enums/userRoles.enums.js';
-import { response_404, response_400, response_500 } from '../utils/responseCodes.utils.js';
+import { response_404, response_403, response_500 } from '../utils/responseCodes.utils.js';
 export async function isChannelAdmin(req, res, next) {
     try {
-        const userid = req.body.user;
-        const user = await User.findById(userid._id);
+        const user = req.body.user;
         const channel = await Channel.findById(req.params.id);
         const workspace = await Workspace.findById(channel.workspace._id);
 
@@ -17,7 +15,7 @@ export async function isChannelAdmin(req, res, next) {
         const isAdminChannel = user.channels.some((channel) => channel._id === req.params.id && channel.role === USER_ROLE.ADMIN);
 
         if (!isAdminChannel || workspace.createdBy !== userid._id) {
-            return response_400(res, 'You are not an admin of this channel');
+            return response_403(res, 'You are not authorized to perform this action');
         }
 
         req.body.channel = channel;
