c8f29247d447221ba5f09a4f1cb9c10359094ea4
Jarviss77 <gupta.shubham2404@gmail.com>
2023-12-17 00:29:15 +0530

    Minor Changes

--- src/middlewares/isChannelAdmin.middleware.js
+++ src/middlewares/isChannelAdmin.middleware.js
@@ -1,25 +1,23 @@
 import  Channel  from '../models/channel.models.js';
-import Workspace from '../models/workspace.models.js';
 import USER_ROLE from '../enums/userRoles.enums.js';
 import { response_404, response_403, response_500 } from '../utils/responseCodes.utils.js';
 export async function isChannelAdmin(req, res, next) {
     try {
         const user = req.body.user;
-        const channel = await Channel.findById(req.params.id);
-        const workspace = await Workspace.findById(channel.workspace._id);
+        const channel = await Channel.findById(req.params.id).populate('workspace');
 
         if (!channel) {
             return response_404(res, 'Channel not found');
         }
 
-        const isAdminChannel = user.channels.some((channel) => channel._id === req.params.id && channel.role === USER_ROLE.ADMIN);
+        const isAdminChannel = user.channels.some((channel) => channel._id.toString() === req.params.id && channel.role === USER_ROLE.ADMIN);
 
-        if (!isAdminChannel || workspace.createdBy !== userid._id) {
+        if (!isAdminChannel || channel.workspace.createdBy !== user._id ) {
             return response_403(res, 'You are not authorized to perform this action');
         }
 
         req.body.channel = channel;
-        req.body.workspace = workspace;
+        req.body.workspace = channel.workspace;
         next();
 
 
