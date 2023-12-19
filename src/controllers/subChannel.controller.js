34f2b008ca370c95101fa0c9a6d67abc9b0300e6
Pranav <pranav123bansal@gmail.com>
2023-12-19 15:32:43 +0530

    Changes done

--- src/controllers/subChannel.controller.js
+++ src/controllers/subChannel.controller.js
@@ -69,7 +69,7 @@ exports.deleteSubChannel = async (req, res) => {
 
 exports.getSubChannel = async (req, res) => {
     try {
-      const ChannelId = req.params.id;
+      const ChannelId = req.Channel._id;
       const channel = await Channel.findById(ChannelId).populate("subChannels");
       const subChannels = channel.subChannels;
       function selectFewerProps(x) {
