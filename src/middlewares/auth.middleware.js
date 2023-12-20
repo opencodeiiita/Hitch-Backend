460b4bb6cf11a77fc7384760fc7ae74b82417cc5
automatefreely <automatefreely@gmail.com>
2023-12-20 09:22:25 +0530

    minor edits

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,35 +1,31 @@
-const User = require('../models/user.models.js');
-const jwt = require('jsonwebtoken');
-const {response_404, response_500} = require('../utils/responseCodes.utils.js');
+const User = require("../models/user.models.js");
+const jwt = require("jsonwebtoken");
+const {
+    response_404,
+    response_500,
+} = require("../utils/responseCodes.utils.js");
 
- async function verifyUser(req, res, next)
-{
+async function verifyUser(req, res, next) {
     const authToken = req.cookies.token || req.token;
 
     console.log(authToken);
-    if (!authToken)
-    {
+    if (!authToken) {
         return response_404(res, "No token provided");
     }
 
-    try
-    {
+    try {
         const decoded = jwt.verify(authToken, process.env.JWT_KEY);
-        req.userId = decoded._id;
 
         const user = await User.findById(req.userId);
 
-        if (!user)
-        {
+        if (!user) {
             return response_404(res, "User not found");
         }
 
-        req.user = user;
+        req.body.user = user;
 
         next();
-    }
-    catch(err)
-    {
+    } catch (err) {
         return response_500(res, "Failed to authenticate User", err);
     }
 }
