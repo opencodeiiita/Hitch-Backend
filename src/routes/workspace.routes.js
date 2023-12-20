3ac07bca53273982aa53a3a028cbbec071ae9307
Dikshant <dikkpsd@gmail.com>
2023-12-20 11:59:21 +0530

    Added delete workspace route

--- src/routes/workspace.routes.js
+++ src/routes/workspace.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const isAuthorized = require("../middlewares/auth.middleware");
-const { createWorkspace, updateWorkspace } = require("../controllers/workspace.controller");
+const { createWorkspace, updateWorkspace, deleteWorkspace } = require("../controllers/workspace.controller");
 const { isWorkspaceAdmin } = require("../middlewares/isWorkspaceAdmin.middleware");
 const router = express.Router();
 
@@ -8,3 +8,4 @@ module.exports = router;
 
 router.post('/create',isAuthorized, createWorkspace)
 router.put('/update/:id', isAuthorized,isWorkspaceAdmin, updateWorkspace);
+router.delete('/delete/:id', isAuthorized, isWorkspaceAdmin, deleteWorkspace);
