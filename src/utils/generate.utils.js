02cd13aa1fe6189c431970dd27355cdfbbe2a3f4
Harshal Gainer <harshalgainer5167@gmail.com>
2024-01-03 00:19:49 +0530

    randomPass

--- src/utils/generate.utils.js
+++ src/utils/generate.utils.js
@@ -1,5 +1,11 @@
 const genrateRandomPassword = (size) => { 
-    // generate random password
+    let randomPass=''
+    const availableChar='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ124567890!@#$%^&*'
+    for(let i=0; i<size; i++){
+        const ind=Math.floor(Math.random*availableChar.length())
+        randomPass+=availableChar.charAt(ind)
+    }
+    return randomPass
 }
 
 module.exports = {
