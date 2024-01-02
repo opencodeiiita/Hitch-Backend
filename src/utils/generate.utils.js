3de65f0338f7634f799f443264651c75ec9e3f42
Jarviss77 <gupta.shubham2404@gmail.com>
2024-01-03 01:57:19 +0530

    Revert "Added RandomPasswordGenerator function"
    This reverts commit 668e6f8730130cae250581cd2fcdd76c8ea15522.

--- src/utils/generate.utils.js
+++ src/utils/generate.utils.js
@@ -1,17 +1,5 @@
-const genrateRandomPassword = (size) => {
+const genrateRandomPassword = (size) => { 
     // generate random password
-    let password = "";
-    const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-    const numbers = "0123456789";
-    const symbols = "!@#$%^&*()_+=";
-    const all = upperCaseLetters + lowerCaseLetters + numbers + symbols;
-
-    for (let i = 0; i < size; i++) {
-        password += all.charAt(Math.floor(Math.random() * all.length));
-    }
-
-    return password;
 }
 
 module.exports = {
