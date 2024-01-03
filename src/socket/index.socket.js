f8e590d749930845baed079f97922f0747e68ac1
Dikshant <dikkpsd@gmail.com>
2024-01-04 00:29:55 +0530

    Added the middleware in io

--- src/socket/index.socket.js
+++ src/socket/index.socket.js
@@ -1,10 +1,46 @@
+import { JsonWebTokenError } from "jsonwebtoken";
 import ChatEventEnum from "../enums/chatEvents.enums";
-import isAuthorized from "../middlewares/auth.middleware";
+const User = require("../models/user.models.js");
 
-const initializeSocketIO = (io ) => {
+import isAuthorized from "../middlewares/auth.middleware";
+const jwt = require("jsonwebtoken");
+
+
+
+
+
+
+const initializeSocketIO = ( ) => {
 
     // to check authToken from cookies
-    io.engine.use(isAuthorized);
+    io.use(async (socket, next) => {
+        const authToken = socket.handshake.headers?.cookie?.token || socket.handshake?.auth?.token;
+
+        if (!authToken) {
+
+            socket.disconnect(true); // to disconnect the user if he is invalid
+            return response_404(res, "No token provided");
+        }
+    
+        try {
+            const decoded = jwt.verify(authToken, process.env.JWT_KEY);
+            // console.log(decoded._id);
+            // return;
+    
+            const user = await User.findById(decoded._id);
+    
+            if (!user) {
+                return response_404(res, "User not found");
+            }
+    
+            socket.user = user;
+         
+            next();
+        } catch (err) {
+            socket.disconnect(true);
+            return response_500(res, "Failed to authenticate User", err);
+        }
+    });
 
 
     io.on("connection", (socket)=>{
