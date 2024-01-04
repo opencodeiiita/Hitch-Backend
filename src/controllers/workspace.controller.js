05efd39e4f003631e632b064a12630190e0065de
automatefreely <automatefreely@gmail.com>
2024-01-04 23:30:08 +0530

    added get workspace api

--- src/controllers/workspace.controller.js
+++ src/controllers/workspace.controller.js
@@ -10,8 +10,8 @@ const {
 
 exports.createWorkspace = async (req, res) => {
     try {
-        const { name, description, workspaceId, user} = req.body;
-        if (!name || !description || !workspaceId ) {
+        const { name, description, workspaceId, user } = req.body;
+        if (!name || !description || !workspaceId) {
             return response_400(
                 res,
                 "Invalid Request: Missing required fields"
@@ -37,9 +37,9 @@ exports.createWorkspace = async (req, res) => {
 
         const savedWorkspace = await workspace.save();
         const DefaultChannel = new Channel({
-            name:"General",
-            workspace:workspace._id.toString(),
-            createdBy:user._id,
+            name: "General",
+            workspace: workspace._id.toString(),
+            createdBy: user._id,
         });
         const savedChannel = await DefaultChannel.save();
         const updatedWorkspace = await Workspace.findByIdAndUpdate(
@@ -64,36 +64,34 @@ exports.updateWorkspace = async (req, res) => {
     try {
         const { name, description, workspaceId } = req.body;
 
-        if (!name && !description && !workspaceId  ) 
-        {
-            return response_400(
-                res,
-                "Invalid Request: No field to update"
-            );
+        if (!name && !description && !workspaceId) {
+            return response_400(res, "Invalid Request: No field to update");
         }
 
         const workspaceIdExists = await Workspace.findOne({
             workspaceId: workspaceId,
         }).exec();
 
-        if(workspaceIdExists)
-        {
+        if (workspaceIdExists) {
             return response_400(
                 res,
                 "Invalid Request: workspaceId already in use"
             );
         }
 
-        const workspace = await Workspace.findByIdAndUpdate(req.body.workspace._id, {
-            name: name,
-            description: description,
-            workspaceId: workspaceId,
-        });
+        const workspace = await Workspace.findByIdAndUpdate(
+            req.body.workspace._id,
+            {
+                name: name,
+                description: description,
+                workspaceId: workspaceId,
+            }
+        );
 
         return response_200(res, "Workspace Updated Successfully", {
-            name: workspace.name ,
+            name: workspace.name,
             description: workspace.description,
-            workspaceId: workspace.workspaceId ,
+            workspaceId: workspace.workspaceId,
             id: workspace._id,
         });
     } catch (err) {
@@ -107,5 +105,61 @@ exports.deleteWorkspace = async (req, res) => {
         return response_200(res, "Workspace Deleted Successfully");
     } catch (err) {
         return response_500(res, "Error deleting workspace", err);
+    }
+};
+
+exports.getWorkspace = async (req, res) => {
+    try {
+        const userId = req.body.user._id;
+        const channelsAdmin = await Channel.find({ createdBy: userId }).exec();
+        // get all the channels in which user is present in channel members array
+        const channelsMember = await Channel.find({
+            members: { $elemMatch: { user: userId } },
+        }).exec();
+
+        const workspace = [];
+        for (let channel of channelsAdmin) {
+            const workspaceId = channel.workspace;
+            const workspaceDetails =
+                await Workspace.findById(workspaceId).populate("channels");
+            workspace.push(workspaceDetails);
+        }
+
+        for (let channel of channelsMember) {
+            const workspaceId = channel.workspace;
+            const workspaceDetails =
+                await Workspace.findById(workspaceId).populate("channels");
+            workspace.push(workspaceDetails);
+        }
+
+        // remove duplicate workspaces
+        const uniqueWorkspaces = Array.from(
+            new Set(workspace.map((a) => a._id))
+        ).map((_id) => {
+            return workspace.find((a) => a._id === _id);
+        });
+        // Get required fields from workspace
+        const requiredWorkspaces = uniqueWorkspaces.map((workspace) => {
+            return {
+                name: workspace.name,
+                description: workspace.description,
+                workspaceId: workspace.workspaceId,
+                id: workspace._id,
+                channels: workspace.channels.map((channel) => {
+                    return {
+                        name: channel.name,
+                        description: channel.description,
+                        id: channel._id,
+                    };
+                }),
+            };
+        });
+        return response_200(
+            res,
+            "Workspace fetched successfully",
+            requiredWorkspaces
+        );
+    } catch (err) {
+        return response_500(res, "Error getting workspace", err);
     }
 };
--- src/routes/workspace.routes.js
+++ src/routes/workspace.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const isAuthorized = require("../middlewares/auth.middleware");
-const { createWorkspace, updateWorkspace, deleteWorkspace } = require("../controllers/workspace.controller");
+const { createWorkspace, updateWorkspace, deleteWorkspace, getWorkspace } = require("../controllers/workspace.controller");
 const { isWorkspaceAdmin } = require("../middlewares/isWorkspaceAdmin.middleware");
 const router = express.Router();
 
@@ -9,3 +9,4 @@ module.exports = router;
 router.post('/create',isAuthorized, createWorkspace)
 router.put('/update/:id', isAuthorized,isWorkspaceAdmin, updateWorkspace);
 router.delete('/delete/:id', isAuthorized, isWorkspaceAdmin, deleteWorkspace);
+router.get('/get', isAuthorized, getWorkspace);
