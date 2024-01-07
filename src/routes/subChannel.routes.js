3bb90728508576653fbcaadebe8189a2a7d4b4f2
Harshal Gainer <harshalgainer5167@gmail.com>
2024-01-08 00:43:31 +0530

    get subchannel route

--- src/routes/subChannel.routes.js
+++ src/routes/subChannel.routes.js
@@ -6,6 +6,7 @@ const {
 } = require('../controllers/subChannel.controller');
 const { isChannelAdmin } = require('../middlewares/isChannelAdmin.middleware');
 const { isSubChannelAdmin } = require('../middlewares/isSubChannelAdmin.middleware');
+const { isChannelMember}=require('../middlewares/isChannelMember.middleware')
 const isAuthorized = require('../middlewares/auth.middleware');
 
 const express = require('express');
@@ -16,4 +17,4 @@ module.exports = router;
 router.post('/create/in/:id', [isAuthorized, isChannelAdmin], createSubChannel);
 router.put('/update/:id', [isAuthorized,isSubChannelAdmin ],updateSubChannel);
 router.delete('/delete/:id', [isAuthorized, isSubChannelAdmin], deleteSubChannel);
-router.get('/get/:id',[isAuthorized,isChannelAdmin], getSubChannel);
+router.get('/get/:id',[isAuthorized,isChannelMember], getSubChannel);
