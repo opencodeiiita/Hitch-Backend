086b07dcbf58701e1b3dd012e8292df246ef5311
Swaroop Dora <105218991+automatefreely@users.noreply.github.com>
2024-01-07 23:46:21 +0530

    Revert "Update generate.utils.js"

--- src/utils/generate.utils.js
+++ src/utils/generate.utils.js
@@ -1,17 +1,7 @@
 const genrateRandomPassword = (size) => { 
     // generate random password
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+';
-    let password = '';
-
-    for (let i = 0; i < size; i++)
-    {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        password += characters.charAt(randomIndex);
-    }
-
-    return password;
-};
+}
 
 module.exports = {
     genrateRandomPassword
-};
+}
