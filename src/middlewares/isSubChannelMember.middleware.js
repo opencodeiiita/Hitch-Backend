21647e8a66e18c86c988ce290a8add685f7051f4
Jarviss77 <gupta.shubham2404@gmail.com>
2024-01-12 00:13:54 +0530

    Changes Done

--- src/middlewares/isSubChannelMember.middleware.js
+++ src/middlewares/isSubChannelMember.middleware.js
@@ -1,18 +1,22 @@
 const SubChannel = require('../models/subChannel.models.js');
+const Channel = require('../models/channel.models.js');
 const {response_403, response_404, response_500} = require('../utils/responseCodes.utils.js');
 
 exports.isSubChannelMember= async(req, res, next)=>{
     try {
         const userId = req.body.user._id;
         const subChannelId = req.params.id;
-        const subChannel = await SubChannel.findById(subChannelId).populate('channel').populate('channel.workspace');
+        const subChannel = await SubChannel.findById(subChannelId).populate('channel');
+        const workspace = await Channel.findById(subChannel.channel._id).populate('workspace');
+
         if(!subChannel) {
             return response_404(res, 'SubChannel not found');
         }
 
         const channelMember = subChannel.channel.members.find(member => member._id === userId);
-        const workspaceAdmin = subChannel.channel.workspace.createdBy === userId;
-        if(!channelMember && !workspaceAdmin) {
+        const channelAdmin = subChannel.channel.createdBy.equals(userId);
+        const workspaceAdmin = workspace.createdBy.equals(userId);
+        if(!channelMember && !workspaceAdmin && !channelAdmin) {
             return response_403(res, 'You are not authorized to perform this action');
         }
 
--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -2,9 +2,10 @@ const { getAllMessages } = require("../controllers/message.controller");
 const isAuthorized  = require("../middlewares/auth.middleware");
 
 const express = require("express");
+const {isSubChannelMember} = require("../middlewares/isSubChannelMember.middleware");
 
 const router = express.Router();
 
-router.get("/getAll/in/:id", isAuthorized, getAllMessages);
+router.get("/getAll/in/:id", [isAuthorized, isSubChannelMember], getAllMessages);
 
 module.exports = router;
